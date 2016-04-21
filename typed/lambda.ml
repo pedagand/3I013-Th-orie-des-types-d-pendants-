fa86e813850ba301e569dca136765e09919be788
Pierre-Évariste Dagand <pierre-evariste.dagand@inria.fr>
2016-04-21 19:56:44 +0200

    Todo

--- typed/lambda.ml
+++ typed/lambda.ml
@@ -486,9 +486,11 @@ let rec check contexte inT ty
 	 | _ -> failwith "Type of a pair must be a Croix"
        end 
 (*=End *)
-(*=synth_ann *)
+(*=synth_def *)
 and synth contexte exT 
     = match exT with
+(*=End *)
+(*=synth_ann *)
     | Ann(tm, ty) ->
        if check contexte tm ty then
          ty 
